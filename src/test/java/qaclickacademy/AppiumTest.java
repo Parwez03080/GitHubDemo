8d19b44ad5b1cff7dc287bf3c61f4808f44fc9d1
parwez.hayat <parwezhayat81@yahoo.com>
2022-06-09 03:07:16 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void appium()
+	{
+		System.out.println("AppiumTest");
+	}
+	public  void Assert()
+	{
+		org.testng.Assert.assertTrue(true);
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void SelenoumTest()
+	{
+		Assert.assertTrue(true);
+	}
+	@Test
+	public void elementsui()
+	{
+		System.out.println("elementsui");
+	}
+
+}
